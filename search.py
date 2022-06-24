6c74cbefe3a78c17135f4c87e94cb1fa01851b03
Maximum159 <maxwellvale@icloud.com>
2022-06-24 10:29:28 -0500

    added multiple terms functionality

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ import argparse
 import os
 from os.path import isfile, join
 network =input("which network to use? googlenet/resnet-18\n")
-target = input("What should the classified photos contain?\n")
+target = input("What should the classified photos contain?\neach object should be seperated by a comma\n").split(",")
 file_names_raw=os.listdir(".")
 file_names=[]
 for f in file_names_raw:
@@ -15,11 +15,14 @@ net=jetson.inference.imageNet(network)
 matching_files=[]
 non_matches=[]
 for f in file_names:
+	added=False
 	img=jetson.utils.loadImage(f)
 	class_idx, confidence = net.Classify(img)
-	if((target in net.GetClassDesc(class_idx) or net.GetClassDesc(class_idx) in target) and confidence>=0.5):
-		matching_files.append(f)
-	else:
+	for term in target:
+		if((term in net.GetClassDesc(class_idx) or net.GetClassDesc(class_idx) in term) and confidence>=0.5):
+			matching_files.append(f)
+			added=True
+	if(added==False):
 		non_matches.append(f)
 print("matching files: "+str(matching_files))
 print("non-matching files: "+str(non_matches))
